837c92bd44ebdd4c41f74789691208495ad88db7
X-Elie-X <103565978+X-Elie-X@users.noreply.github.com>
2022-09-21 10:44:17 +0200

    Create Home.js

---
+++ src/components/Home.js
@@ -0,0 +1,23 @@
+function Home() {
+  return (
+    <>
+      <h2 className="titel1">Welcome to our page!</h2>
+      <p className="paragraph-Home">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat natus iste quos atque
+        error ducimus ipsam, quia sequi saepe accusamus unde. Illum mollitia soluta dicta. Possimus
+        architecto nihil eaque unde.Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+        Quaerat natus iste quos atque error ducimus ipsam, quia sequi saepe accusamus unde. Illum
+        mollitia soluta dicta. Possimus architecto nihil eaque unde.
+      </p>
+      <p className="paragraph-Home">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat natus iste quos atque
+        error ducimus ipsam, quia sequi saepe accusamus unde. Illum mollitia soluta dicta. Possimus
+        architecto nihil eaque unde.Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+        Quaerat natus iste quos atque error ducimus ipsam, quia sequi saepe accusamus unde. Illum
+        mollitia soluta dicta. Possimus architecto nihil eaque unde.
+      </p>
+    </>
+  );
+}
+
+export default Home;
